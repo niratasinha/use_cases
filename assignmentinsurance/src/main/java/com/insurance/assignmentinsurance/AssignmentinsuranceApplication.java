f9cf20d4c42ff075e2166f6e0eabb0f28c78ce20
niratasinha <nirata.sinha@kpipartners.com>
2022-10-20 18:45:19 +0530

    commit for Insurance-Assignment

---
+++ assignmentinsurance/src/main/java/com/insurance/assignmentinsurance/AssignmentinsuranceApplication.java
@@ -0,0 +1,13 @@
+package com.insurance.assignmentinsurance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssignmentinsuranceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssignmentinsuranceApplication.class, args);
+	}
+
+}
---
+++ assignmentinsurance/src/test/java/com/insurance/assignmentinsurance/AssignmentinsuranceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.insurance.assignmentinsurance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignmentinsuranceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
